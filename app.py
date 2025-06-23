6aef4bbe45a176ec23d90b51d9e5bbb8898a048f
anaseller <seller.anst@gmail.com>
2025-06-23 12:48:48 +0200

    new commit

--- app.py
+++ app.py
@@ -2,20 +2,7 @@
 from flask import Flask
 from src.core.app_runner import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-
 if __name__ == '__main__':
     app = create_app()
+
     app.run()
-
-
-
-
-# if __name__ == '__main__':
-#     app.run()
